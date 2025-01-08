a934cb8c3e09d24c96809dc0575c263436f7e803
Viji Ravi <Vijayalakshmi@AX15>
2025-01-08 10:47:19 -0500

    commit 02

---
+++ src/test/java/allure_test/Testcase4.java
@@ -0,0 +1,14 @@
+package allure_test;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Testcase4 {
+  @Test
+  public void testmethod4() {
+	  Assert.assertTrue(false);
+	  System.out.println("testmethod4 is not working as expected");
+  }
+}
---
+++ src/test/java/allure_test/Testcase5.java
@@ -0,0 +1,14 @@
+package allure_test;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Testcase5 {
+  @Test
+  public void testmethod5() {
+	  Assert.assertTrue(false);
+	  System.out.println("testmethod5 is not working as expected");
+  }
+}
