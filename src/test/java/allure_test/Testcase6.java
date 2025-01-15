b6dde8c739b5c49bd3bf9e91de79af0c64cbcb97
Viji Ravi <Vijayalakshmi@AX15>
2025-01-15 12:00:56 -0500

    commit jan 15th 03

---
+++ src/test/java/allure_test/Testcase6.java
@@ -0,0 +1,14 @@
+package allure_test;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Testcase6 {
+  @Test
+  public void testmethod6() {
+	  Assert.assertTrue(true);
+	  System.out.println("testmethod6 is  working as expected");
+  }
+}
